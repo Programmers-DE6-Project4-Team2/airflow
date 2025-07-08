167c9594291c6f67c8736731b25a928885cb96e4
dawit <35230675+dawit0905@users.noreply.github.com>
2025-07-08 18:29:02 +0900

    Feat/oliveyoung product fetch dag (#9)
    * feat: add bronze_oliveyoung_product_fetch_daily.py
    - 카테고리들을 불러와서 Dynamic task mapping으로 실행
    
    * feat:
    - dag에 owner 추가
    - utc 타임존 명시
    
    * feat: /dags 에 있는 모든 파일과 requirements.txt도 배포되게끔 추가
    
    * feat: 로컬 환경에서 requirements.txt로 패키지 다운로드
    
    * feat: requirements.txt 추가

---
+++ dags/bronze_oliveyoung_product_fetch_daily/bronze_oliveyoung_product_fetch_daily.py
@@ -0,0 +1,56 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.operators.cloud_run import CloudRunExecuteJobOperator
+from common.oliveyoung_product_catagories import get_all_categories
+import pendulum
+
+CATEGORY_DICT = get_all_categories()
+CATEGORY_LIST = [
+    {
+        "category_name": name,
+        "category_url": url,
+        "max_pages": 5,
+    }
+    for name, url in CATEGORY_DICT.items()
+]
+
+with DAG(
+    dag_id="bronze_oliveyoung_product_fetch_daily",
+    schedule_interval="0 0 * * *",  # 매일 00:00 UTC → KST 오전 9시
+    start_date=pendulum.now('UTC').subtract(days=1),
+    catchup=False,
+    tags=["oliveyoung", "cloud-run"],
+    default_args={
+        'owner': 'dawit0905@gmail.com',
+        "retries": 1,
+        "retry_delay": timedelta(minutes=3),
+    },
+) as dag:
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+
+    run_cloud_run_job = CloudRunExecuteJobOperator.partial(
+        task_id="run_cloud_run_job",
+        project_id="de6-2ez",
+        region="asia-northeast3",
+        job_name="oliveyoung-product-crawler-job",
+    ).expand(
+        overrides=[
+            {
+                "container_overrides": [
+                    {
+                        "env": [
+                            {"name": "CATEGORY_NAME", "value": category["category_name"]},
+                            {"name": "CATEGORY_URL", "value": category["category_url"]},
+                            {"name": "MAX_PAGES", "value": str(category["max_pages"])},
+                        ]
+                    }
+                ]
+            }
+            for category in CATEGORY_LIST
+        ]
+    )
+
+    start >> run_cloud_run_job >> end
