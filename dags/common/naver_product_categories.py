313290330206afedbb87205d0fbf7c8c9eb97026
jerygen <myksphone2001@gmail.com>
2025-07-11 16:50:02 +0900

    feat: 네이버 상품 데잍리 수집 dag 추가 및 카테고리 저장 파일 추가 (#13)
    Co-authored-by: 김미연 <miyeon@gimmiyeon-ui-MacBookAir.local>

---
+++ dags/common/naver_product_categories.py
@@ -0,0 +1,18 @@
+# naver_product_categories.py
+def get_all_product_categories():
+    return [
+        "all",
+        "skincare",
+        "suncare",
+        "mask_pack",
+        "cleansing",
+        "makeup",
+        "nailcare",
+        "haircare",
+        "bodycare",
+        "beautytools",
+        "perfume",
+        "mens",
+        "beauty_device",
+        "kids",
+    ]
