4c9da2d43a84949cc8df153fafd5ee2fdee96542
jerygen <myksphone2001@gmail.com>
2025-07-20 17:02:22 +0900

    feat: add naver review crawler DAG and gold prodcut layer (#52)
    Co-authored-by: 김미연 <miyeon@gimmiyeon-ui-MacBookAir.local>

---
+++ dags/gold-layer/gold_products.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    "owner": "myksphone2001@gmail.com",
+    "retries": 1,
+}
+
+with DAG(
+    dag_id="gold_products_dbt_run",
+    start_date=days_ago(1),
+    schedule_interval=None,  # 수동 실행 또는 상위 DAG에서 Trigger
+    catchup=False,
+    tags=["gold", "dbt", "product"]
+) as dag:
+
+    run_dbt_gold_products = BashOperator(
+        task_id="run_dbt_gold_products_model",
+        bash_command="""
+        source /home/airflow/airflow/dbt-venv/bin/activate && \
+        dbt run \
+            --project-dir /home/airflow/airflow/dbt/beauty_elt \
+            --profiles-dir /home/airflow/.dbt \
+            --select gold_products
+        """,
+    )
