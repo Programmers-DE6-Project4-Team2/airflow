5bd4bebc748a5431f85f3e2e43f7df78363b53d8
‘h2k9971’ <h2k9971@naver.com>
2025-07-23 16:05:59 +0900

    feat: autodetect를 false로 지정 후 수동 저장

--- dags/bronze-layer/bronze_oliveyoung_reviews.py
+++ dags/bronze-layer/bronze_oliveyoung_reviews.py
@@ -1,9 +1,9 @@
 from datetime import timedelta
 from airflow import DAG
 from airflow.decorators import task
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from google.cloud import bigquery
 
 default_args = {
@@ -15,7 +15,7 @@ default_args = {
 with DAG(
     dag_id="bronze_oliveyoung_reviews",
     start_date=days_ago(1),
-    schedule_interval="30 14 * * *", # 23시 30분 실행
+    schedule_interval="0 4 * * *",
     catchup=True,
     default_args=default_args,
     description="Load OliveYoung review CSVs from GCS to BigQuery Bronze in a single batch",
@@ -53,7 +53,27 @@ with DAG(
             skip_leading_rows=1,
             write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
             allow_quoted_newlines=True,
-            autodetect=True,
+            autodetect=False,
+            schema=[
+                bigquery.SchemaField("review_id", "INTEGER"),
+                bigquery.SchemaField("product_name", "STRING"),
+                bigquery.SchemaField("star", "STRING"),
+                bigquery.SchemaField("review", "STRING"),
+                bigquery.SchemaField("skin_type", "STRING"),
+                bigquery.SchemaField("date", "STRING"),
+                bigquery.SchemaField("purchase_type", "STRING"),
+                bigquery.SchemaField("page", "INTEGER"),
+                bigquery.SchemaField("helpful", "STRING"),  # 중요: "999+" 때문에 INT → STRING
+                bigquery.SchemaField("scraped_at", "TIMESTAMP"),
+                bigquery.SchemaField("total_review_count", "INTEGER"),
+                bigquery.SchemaField("product_id", "STRING"),
+                bigquery.SchemaField("product_url", "STRING"),
+                bigquery.SchemaField("category_name", "STRING"),
+                bigquery.SchemaField("crawling_timestamp", "TIMESTAMP"),
+                bigquery.SchemaField("crawling_date", "DATE"),
+                bigquery.SchemaField("source", "STRING"),
+                bigquery.SchemaField("data_type", "STRING"),
+            ]
         )
 
         load_job = bq_client.load_table_from_uri(
@@ -63,15 +83,14 @@ with DAG(
         )
         load_job.result()
         print(f"✅ Successfully loaded {len(uris)} files into BigQuery")
-
+        
     file_list = list_review_csv_files()
     load_task = load_csvs_to_bq(file_list)
 
-    # dbt trigger 예: 나중에 사용하려면 여기에 연결
-    trigger_dbt = TriggerDagRunOperator(
+    trigger_silver_dag = TriggerDagRunOperator(
         task_id="trigger_silver_oliveyoung_review_dbt",
         trigger_dag_id="silver_oliveyoung_review_dbt",
         wait_for_completion=False,
     )
-
-    load_task >> trigger_dbt
+    
+    load_task >> trigger_silver_dag
