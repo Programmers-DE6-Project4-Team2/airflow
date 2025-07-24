b0c49fb1543b024798638f339fb021cf9b70c104
김미연 <miyeon@gimmiyeon-ui-MacBookAir.local>
2025-07-24 14:55:17 +0900

    feat/gold-products-schedule-change

--- dags/gold-layer/gold_products.py
+++ dags/gold-layer/gold_products.py
@@ -10,7 +10,7 @@ default_args = {
 with DAG(
     dag_id="gold_products_dbt_run",
     start_date=days_ago(1),
-    schedule_interval="0 15 * * *", # 매일 오후 3시 UTC → 한국 시간 기준 자정
+    schedule_interval = "30 3 * * *", # 매일 오전 3시 30분 UTC → 한국 시간 기준 정오 12시 30분 
     catchup=False,
     tags=["gold", "dbt", "product"]
 ) as dag:
